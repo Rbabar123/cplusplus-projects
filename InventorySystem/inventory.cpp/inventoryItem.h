567685620c53444b6fff8deebd4ac6c8de919313
Rbabar123 <108402307+Rbabar123@users.noreply.github.com>
2025-06-24 18:07:53 +0800

    Add files via upload

---
+++ InventorySystem/inventory.cpp/inventoryItem.h
@@ -0,0 +1,13 @@
+// InventoryItem.h
+#ifndef INVENTORY_ITEM_H
+#define INVENTORY_ITEM_H
+
+#include <string>
+
+struct InventoryItem {
+    std::string name;
+    int quantity;
+    double price;
+};
+
+#endif
